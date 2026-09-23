08f851e0156c806db7d3b1e14b46f7dc4556b78f
sasankit <shashankacharya75@gmail.com>
2026-09-23 22:12:43 +0200

    updated day 01

--- Selflearn/Day01.py
+++ Selflearn/Day01.py
@@ -3,22 +3,34 @@
 
 #Date : 22-07-2026
 
-message  = "Hello World"
-print(message[2:4]) #index slicing
+# message  = "Hello World"
+# print(message[2:4]) #index slicing
 
-#String Method
-"""""
-print(message.lower())
-print(message.upper())
+# #String Method
+# print(message.lower())
+# print(message.upper())
 
-"""
-#Using f string
-getname = input("Enter your name: ")
-print(f"""Hello {getname},
-      How are you doing ?
-      Best,
-      Team""")
+# #Using f string
+# getname = input("Enter your name: ")
+
+# print(f"""Hello {getname},
+# How are you doing?
+# Best,
+# Team
+# """)
+
 
 # using format methodd
-print ("{} Welcome".format(getname))
+#print ("Welcome {}".format(getname))
 
+
+
+#Date : 23.07.2026
+
+#num = 5
+#print(type(num))
+
+num1 = '100'
+num2 = '200'
+
+print(num1 + num2)
